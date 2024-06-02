0ec3495e4c111f75ee36e73004bfb4f1c92440b3
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-06-02 15:28:31 -0300

    Add sneakerData

--- kickshub/src/App.js
+++ kickshub/src/App.js
@@ -1,27 +1,45 @@
+const sneakerData = [
+  {
+    id: 1,
+    photo: 'assets/images/nike-max-90-black-blue.jpg',
+    altText: 'Nike Air Max 90 Black and Blue',
+    name: 'Nike Air Max 90',
+    description:
+      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    price: '$ 150.00',
+  },
+  {
+    id: 2,
+    photo: 'assets/images/nike-max-excee-black-white.jpg',
+    altText: 'Nike Air Max Excee Black and White',
+    name: 'Nike Air Max Excee',
+    description:
+      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    price: '$ 199.00',
+  },
+];
+
 export default function App() {
   return (
     <div className='app'>
-      <Card />
+      {sneakerData.map((sneaker) => (
+        <Card sneaker={sneaker} key={sneaker.id} />
+      ))}
     </div>
   );
 }
 
-function Card() {
+function Card({ sneaker }) {
   return (
     <div className='card__wrapper'>
       <div>
-        <img
-          src='assets/images/nike-max-90-black-blue.jpg'
-          alt='Nike Air Max 90 Black and Blue'
-        ></img>
+        <img src={sneaker.photo} alt={sneaker.altText}></img>
       </div>
       <div className='card__info'>
-        <h2>Nike Air Max 90</h2>
-        <p>
-          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-          consectetur, adipisci velit...
-        </p>
-        <button>Add</button>
+        <h2>{sneaker.name}</h2>
+        <p>{sneaker.description}</p>
+        <p className='card__price'>{sneaker.price}</p>
+        <button className='card__btn'>Add to cart</button>
       </div>
     </div>
   );
