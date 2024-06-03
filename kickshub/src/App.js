387cd1aecf279367fdb4a0164feba25f6d6b1b36
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-06-03 09:59:21 -0300

    Update index.css

--- kickshub/src/App.js
+++ kickshub/src/App.js
@@ -14,7 +14,7 @@ export default function App() {
 function Card({ sneaker }) {
   return (
     <div className='card__wrapper'>
-      <div>
+      <div className='card__img'>
         <img src={sneaker.photo} alt={sneaker.altText}></img>
       </div>
       <div className='card__info'>
@@ -30,17 +30,17 @@ function Card({ sneaker }) {
 function SneakerCard() {
   return (
     <div className='sneakerCard'>
-      <div>
+      <div className='sneakerCard__img'>
         <img
           src='assets/images/nike-max-90-black-blue.jpg'
           alt='Nike Air Max 90 Black and Blue'
         ></img>
       </div>
       <div className='sneakerCard__info'>
-        <h2>'Nike Air Max 90'</h2>
+        <h2>Nike Air Max 90</h2>
         <p>
-          'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
-          consectetur, adipisci velit...'
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+          consectetur, adipisci velit...
         </p>
         <div className='sneakerCard__colors'></div>
         <p className='sneakerCard__sizes'>36 37 38 39 40 41 42</p>
