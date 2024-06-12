eba3212d77f76960fabed764a1a477bcb465ddd4
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-06-12 08:29:17 -0300

    Initial commit

---
+++ ex-01/src/App.js
@@ -0,0 +1,70 @@
+/* import { data } from './data';
+import { useState } from 'react'; */
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <SneakerCard />
+      </main>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <div className='logo'>
+        <img
+          src='./assets/images/sneaker-logo.png'
+          alt='Sneaker with wings'
+        ></img>
+      </div>
+
+      <div className='icon-cart'>
+        <i className='bx bx-cart'></i>
+        <span>0</span>
+      </div>
+    </header>
+  );
+}
+
+function SneakerCard() {
+  return (
+    <div className='sneakerCard'>
+      <div className='sneakerCard__img'>
+        <img
+          src='./assets/images/nike-max-90-black-blue.jpg'
+          alt='Nike Air Max 90 Black and Blue'
+        ></img>
+      </div>
+      <div className='sneakerCard__info'>
+        <h2>Name</h2>
+        <p>Description</p>
+        <div className='color-wrapper'>
+          <p>Colors</p>
+
+          <div className='sneakerCard__colors'></div>
+        </div>
+        <div className='size-wrapper'>
+          <p>Sizes</p>
+          <p className='sneakerCard__sizes'>
+            <span></span>
+          </p>
+        </div>
+        <div className='sneakerCard__quantity-price'>
+          <div className='quantity__wrapper'>
+            <button>-</button>
+            <input type='number'></input>
+            <button>+</button>
+          </div>
+          <p className='sneakerCard__price'>Price</p>
+        </div>
+        <button className='sneakerCard__btn'>Add to cart</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex-01/src/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    blackBlue: {
+      img: 'assets/images/nike-max-90-black-blue.jpg',
+      altText: 'Nike Air Max 90 Black and Blue',
+      color: ['#000000', '#22334c'],
+      sizes: [38, 39, 40, 41, 42],
+    },
+    blackWhite: {
+      img: 'assets/images/nike-max-90-black-white.jpg',
+      altText: 'Nike Air Max 90 Black and White',
+      color: ['#000000', '#ffffff'],
+      sizes: [36, 37, 38, 39, 40, 41, 42],
+    },
+    greyYellow: {
+      img: 'assets/images/nike-max-90-grey-yellow.jpg',
+      altText: 'Nike Air Max 90 Grey and Yellow',
+      color: ['grey', 'yellow'],
+      sizes: [36, 37, 38, 39, 40],
+    },
+    white: {
+      img: 'assets/images/nike-max-90-white.jpg',
+      altText: 'Nike Air Max 90 White',
+      color: ['#ffffff', '#ffffff'],
+      sizes: [36, 37, 38, 39, 40],
+    },
+    name: 'Nike Air Max 90',
+    description:
+      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    price: '$ 150.00',
+    allSizes: [36, 37, 38, 39, 40, 41, 42],
+  },
+];
