f721a185b654fd3b92968e4b5144a95e653c7fa8
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-06 08:52:40 -0300

    Update App.js

--- colorChange/src/App.js
+++ colorChange/src/App.js
@@ -26,7 +26,7 @@ function Header({ light, onColorChange }) {
         type='range'
         value={light}
         min='0'
-        max='2'
+        max='100'
         id='light'
       />
     </header>
