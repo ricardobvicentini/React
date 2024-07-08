088e4563d862b15be56c2b76c38ac10f53ce3e56
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-08 13:05:25 -0300

    Update bookData.js

--- my-bookshelf/src/bookData.js
+++ my-bookshelf/src/bookData.js
@@ -1,5 +1,61 @@
 const bookData = [
   { title: 'História da Beleza', author: 'Umberco Eco', topic: 'history' },
+  {
+    title: 'Um Dia "Daqueles" - Uma Lição de Vida para Levantar o seu Astral',
+    author: 'Bradley Trevor Greive',
+    topic: 'Autoajuda',
+  },
+  {
+    title:
+      'Fulminado por um Raio - A História de Marconi e da Telegrafia Sem Fio',
+    author: 'Eric Larson',
+    topic: 'Literatura - Biografia',
+  },
+  {
+    title: 'O que é Pós-Moderno',
+    author: 'Jair Ferreira Dos Santos',
+    topic: 'Sociologia',
+  },
+  {
+    title: 'Sargento Getúlio',
+    author: 'João Ubaldo Ribeiro',
+    topic: 'Literatura Brasileira',
+  },
+  {
+    title: 'Planet Earth',
+    author: 'Jonatan Weiner',
+    topic: 'Ciências - Astronomia',
+  },
+  {
+    title: 'O Universo numa Casca de Noz',
+    author: 'Stephen Hawking',
+    topic: 'Ciências - Cosmologia',
+  },
+  {
+    title: 'Pálido Ponto Azul - Uma Visão do Futuro da Humanidade no Espaço',
+    author: 'Carl Sagan',
+    topic: 'Ciências - Astronomia',
+  },
+  {
+    title: 'Caim',
+    author: 'José saramago',
+    topic: 'Literatura Portuguesa',
+  },
+  {
+    title: 'Armas, Germes e Aço',
+    author: 'Jared Diamond',
+    topic: 'Sociologia',
+  },
+  {
+    title: 'O Espírito das Leis',
+    author: 'Montesquieu',
+    topic: 'Filosofia',
+  },
+  {
+    title: '50 Batalhas que Mudaram o Mundo',
+    author: 'William Weir',
+    topic: 'História Geral',
+  },
 ];
 
 export default bookData;
