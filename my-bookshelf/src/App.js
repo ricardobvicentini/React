e2921afd8f668c0e7b76d4b8faba037649ffa961
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-08 12:53:24 -0300

    Remove files / add bookData

--- my-bookshelf/src/App.js
+++ my-bookshelf/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import bookData from './bookData';
 
 function App() {
   return (
---
+++ my-bookshelf/src/bookData.js
@@ -0,0 +1,5 @@
+const bookData = [
+  { title: 'História da Beleza', author: 'Umberco Eco', topic: 'history' },
+];
+
+export default bookData;
