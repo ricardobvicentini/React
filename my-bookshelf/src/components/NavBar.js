5334d45492416313e700975be2e7c5b449d0a016
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-10 14:56:20 -0300

    Update NavBar.js

--- my-bookshelf/src/components/NavBar.js
+++ my-bookshelf/src/components/NavBar.js
@@ -7,8 +7,18 @@ const NavBar = () => {
         <a className='navbar-brand mx-0' href='/'>
           My Book<span style={{ color: 'red' }}>s</span>helf
         </a>
-        <form className='d-flex' role='search'>
-          {/* <input
+        <button
+          className='navbar-toggler'
+          data-bs-toggle='collapse'
+          data-bs-target='#nav'
+          aria-controls='nav'
+          aria-label='Expand Navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='nav'>
+          <form className='d-flex' role='search'>
+            {/* <input
             className='form-control me-2'
             type='search'
             placeholder='Search'
@@ -17,23 +27,28 @@ const NavBar = () => {
           <button className='btn btn-outline-warning' type='submit'>
             Search
           </button> */}
-          <div class='input-group'>
-            <input
-              type='text'
-              className='form-control'
-              placeholder='Search...'
-              aria-label='Input group example'
-              aria-describedby='btnGroupAddon2'
-            />
-            <div className='input-group-text' id='btnGroupAddon2' type='submit'>
-              <FaSearch />
+            <div class='input-group'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Search...'
+                aria-label='Input group example'
+                aria-describedby='btnGroupAddon2'
+              />
+              <div
+                className='input-group-text'
+                id='btnGroupAddon2'
+                type='submit'
+              >
+                <FaSearch />
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
 
-        <button className='btn btn-warning dropdown-toggle' type='button'>
-          <FaFilter />
-        </button>
+          <button className='btn btn-warning dropdown-toggle' type='button'>
+            <FaFilter />
+          </button>
+        </div>
       </div>
     </nav>
   );
