667eeb178238480e4b9b6dd5de3238034d382d88
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-11 08:21:52 -0300

    Add hero

--- my-bookshelf/src/App.js
+++ my-bookshelf/src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 /* import bookData from './bookData'; */
 
 const App = () => {
   return (
     <div className='App'>
       <NavBar />
+      <Hero />
     </div>
   );
 };
---
+++ my-bookshelf/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div class='hero-wrapper'>
+        <h1>Organize your books quickly and easily</h1>
+        <button type='button' class='btn btn-outline-primary'>
+          Add book
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
