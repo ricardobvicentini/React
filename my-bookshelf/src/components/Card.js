01b0746155df5925717e66f51283d7bba8f744e0
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-12 12:31:38 -0300

    Update Card.js

--- my-bookshelf/src/components/Card.js
+++ my-bookshelf/src/components/Card.js
@@ -1,46 +1,38 @@
 const Card = ({ image, title, author, genre, pages }) => {
   return (
-    <div className='col-12 col-md-6'>
-      <div className='card shadow mb-3' style={{ maxWidth: '33.75rem' }}>
-        <div className='row g-0'>
-          <div className='col-md-4 justify-content-center align-items-center'>
+    <div className='col-12 col-lg-6'>
+      <div
+        className='card shadow h-100 mb-3 mx-auto'
+        style={{ maxWidth: '33.75rem' }}
+      >
+        <div className='row g-0 h-100'>
+          <div className='col-md-4 '>
             <img
               src={image}
-              className='img-fluid img-thumbnail rounded-start'
+              className='img-fluid  rounded-start h-100'
               alt='...'
             />
           </div>
-          <div className='col-md-8'>
-            <div className='card-body'>
+          <div className='col-md-8 h-100'>
+            <div className='card-body h-100 d-flex flex-column'>
               <h5 className='card-title'>{title}</h5>
 
-              <ul className='list-group list-group-flush'>
+              <ul className='list-group list-group-flush mb-2'>
                 <li className='list-group-item'>Author: {author}</li>
                 <li className='list-group-item'>Genre: {genre}</li>
                 <li className='list-group-item'>Pages: {pages}</li>
               </ul>
-              <a href='/' className='btn btn-outline-warning mt-2'>
+              <a
+                href='/'
+                className='btn btn-outline-warning mt-auto'
+                style={{ width: '40%' }}
+              >
                 Edit
               </a>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className='card mx-auto shadow' style={{ width: '20rem' }}>
-        <img src={image} className='card-img-top' alt='...' />
-        <div className='card-body'>
-          <h5 className='card-title'>{title}</h5>
-
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>Author: {author}</li>
-            <li className='list-group-item'>Genre: {genre}</li>
-            <li className='list-group-item'>Pages: {pages}</li>
-          </ul>
-          <a href='/' className='btn btn-outline-warning mt-2'>
-            Edit
-          </a>
-        </div>
-      </div> */}
     </div>
   );
 };
