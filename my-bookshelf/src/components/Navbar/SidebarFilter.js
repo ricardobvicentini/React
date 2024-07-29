a1f90a351f93466983864a7b4a9882a92d7e46d3
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-29 08:37:30 -0300

    Update SidebarFilter.js

--- my-bookshelf/src/components/Navbar/SidebarFilter.js
+++ my-bookshelf/src/components/Navbar/SidebarFilter.js
@@ -59,7 +59,7 @@ const SidebarFilter = ({
                     type='button'
                     data-bs-toggle='collapse'
                     data-bs-target='#collapseOne'
-                    aria-expanded='true'
+                    aria-expanded='false'
                     aria-controls='collapseOne'
                   >
                     Genre
