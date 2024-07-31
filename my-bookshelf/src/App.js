304298c57be71cf64e94ba27a7f6d133ca95ba11
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-07-31 11:16:15 -0300

    Create BookPage > router

--- my-bookshelf/src/App.js
+++ my-bookshelf/src/App.js
@@ -5,9 +5,15 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import BookPage from './pages/BookPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <>
+      <Route index element={<HomePage />} />
+      <Route path='/book' element={<BookPage />} />
+    </>
+  )
 );
 
 const App = () => {
---
+++ my-bookshelf/src/pages/BookPage.js
@@ -0,0 +1,9 @@
+const BookPage = () => {
+  return (
+    <div>
+      <h1>Book Page</h1>
+    </div>
+  );
+};
+
+export default BookPage;
