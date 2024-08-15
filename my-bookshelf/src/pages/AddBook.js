b5395c09ad04653c0daeac812632a28d90e1e299
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-08-15 11:17:50 -0300

    Create AddBook.js

---
+++ my-bookshelf/src/pages/AddBook.js
@@ -0,0 +1,11 @@
+import CommonNavBar from '../components/Navbar/CommonNavBar';
+
+const AddBook = () => {
+  return (
+    <div>
+      <CommonNavBar />
+    </div>
+  );
+};
+
+export default AddBook;
