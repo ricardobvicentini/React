d65f505cf19ab79e66daf6b6be62ba31ad1a15b2
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-08-15 11:18:41 -0300

    Update App.js

--- my-bookshelf/src/App.js
+++ my-bookshelf/src/App.js
@@ -6,12 +6,14 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BookPage from './pages/BookPage';
+import AddBook from './pages/AddBook';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route index element={<HomePage />} />
       <Route path='/book/:id' element={<BookPage />} />
+      <Route path='/add-book' element={<AddBook />} />
     </>
   )
 );
