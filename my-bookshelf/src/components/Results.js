69086087a9d20ce48341020265f69f7eb0d7dd34
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-09-28 06:51:00 -0300

    Update Results.js

--- my-bookshelf/src/components/Results.js
+++ my-bookshelf/src/components/Results.js
@@ -1,8 +1,11 @@
-const Results = ({ query, star, genre, booksToBeFiltered }) => {
+const Results = ({ query, star, genre, room, booksToBeFiltered }) => {
   return (
     <div className='container d-flex justify-content-center align-items-center my-4'>
       <p className='col-3 my-0 text-center' style={{ minHeight: '1.5rem' }}>
-        {(query || star.length !== 0 || genre.length !== 0) &&
+        {(query ||
+          star.length !== 0 ||
+          genre.length !== 0 ||
+          room.length !== 0) &&
           'Results: ' + booksToBeFiltered.length}
       </p>
     </div>
