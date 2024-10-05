faba873727aa0919521dc90b8bc01c0efb943ec4
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-10-05 07:09:30 -0300

    Update LanguageFlags.js

--- my-bookshelf/src/components/Navbar/LanguageFlags.js
+++ my-bookshelf/src/components/Navbar/LanguageFlags.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LanguageFlags = () => {
+  const [selectedFlag, setSelectedFlag] = useState('/assets/images/us.svg');
+
+  const handleSelect = (flag) => {
+    setSelectedFlag(flag);
+  };
+
+  const flags = [
+    { value: '1', src: '/assets/images/us.svg', label: 'USA' },
+    { value: '2', src: '/assets/images/br.svg', label: 'Brazil' },
+  ];
+
   return (
     <div
       className='col d-flex m-auto ms-0 me-3'
-      style={{ maxWidth: '1.5rem', cursor: 'pointer' }}
+      style={{ maxWidth: '5rem', cursor: 'pointer' }}
     >
-      <img
-        className='rounded border border-warning border-2'
-        src='/assets/images/us.svg'
-        alt='Flag'
-      />
+      <div className='dropdown'>
+        <div
+          className='form-select form-select-sm'
+          data-bs-toggle='dropdown'
+          aria-expanded='false'
+        >
+          <img
+            className='rounded border border-warning border-2'
+            src={selectedFlag}
+            alt='Flag'
+            style={{ width: '2rem', height: 'auto' }}
+          />
+        </div>
+        <ul className='dropdown-menu'>
+          {flags.map((flag) => (
+            <li
+              key={flag.value}
+              onClick={() => handleSelect(flag.src)}
+              className='d-flex align-items-center p-2'
+            >
+              <img
+                className='rounded border border-warning border-2 me-2'
+                src={flag.src}
+                alt={flag.label}
+                style={{ width: '1.5rem', height: 'auto' }}
+              />
+              {flag.label}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
